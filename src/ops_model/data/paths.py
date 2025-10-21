210f7dc72e15b410a06c2f13a03312da087020e3
Alexander Hillsley <alexander.hillsley@czbiohub.org>
2025-10-20 17:06:19 -0700

    update cp_features to work with new stitched datasets

--- src/ops_model/data/paths.py
+++ src/ops_model/data/paths.py
@@ -12,18 +12,13 @@ class OpsPaths:
 
         self.base = Path("/hpc/projects/intracellular_dashboard/ops")
 
-        self.phenotyping = (
-            self.base / self.experiment / "3-assembly/phenotyping_og.zarr"
-        )
+        self.phenotyping = self.base / self.experiment / "3-assembly/phenotyping.zarr"
 
         # ensure that well is a valid format
         if well is not None:
             assert len(well) == 2 and "/" not in well
         self.links = (
-            self.base
-            / self.experiment
-            / "3-assembly"
-            / f"{well}_linked_pheno_iss_prob.csv"
+            self.base / self.experiment / "3-assembly" / f"{well}_linked_pheno_iss.csv"
         )
 
         self.cell_profiler_out = (
--- src/ops_model/features/cp_features.py
+++ src/ops_model/features/cp_features.py
@@ -5,9 +5,12 @@ from tqdm import tqdm
 import numpy as np
 import pandas as pd
 from cp_measure.bulk import get_core_measurements
+import torch
 
-from ops_model.data.data_loader import OpsDataManager
+from ops_model.data import data_loader
 from ops_model.data.paths import OpsPaths
+
+torch.multiprocessing.set_sharing_strategy("file_system")
 
 
 def single_channel_features(
@@ -82,28 +85,31 @@ def cp_features(
     CAUTION: should only be run for a single experiment at a time
     """
 
-    data_manager = OpsDataManager(
+    data_manager = data_loader.OpsDataManager(
         experiments=experiment_dict,
         batch_size=1,
-        data_split=(0.1, 0, 0.9),
-        out_channels=["GFP"],
+        data_split=(1, 0, 0),
+        out_channels=["mCherry"],
         initial_yx_patch_size=(256, 256),
-        final_yx_patch_size=(256, 256),
         verbose=verbose,
     )
 
-    data_manager.construct_dataloaders(num_workers=1, dataset_type="cell_profile")
+    data_manager.construct_dataloaders(
+        num_workers=num_workers, dataset_type="cell_profile"
+    )
     train_loader = data_manager.train_loader
+    batch = next(iter(train_loader))
+    print("made a batch")
 
     measurements = get_core_measurements()
     shape_fcn = measurements.pop("sizeshape", None)
 
     def _process_batch(batch):
 
-        img = batch["data"][0].detach().cpu().numpy()  # 1, C, H, W
+        img = batch["data"][0].detach().cpu().numpy()
         mask = batch["mask"][0].detach().cpu().numpy()
         features = single_channel_features(
-            img, mask, prefix="gfp", measurements=measurements
+            img, mask, prefix="mCherry", measurements=measurements
         )
         cell_mask_features = shape_features(
             img, mask, prefix="cell_mask", fcn=shape_fcn
@@ -120,7 +126,7 @@ def cp_features(
 
         return pd.DataFrame(features)
 
-    all_features = Parallel(n_jobs=num_workers)(
+    all_features = Parallel(n_jobs=1)(
         delayed(_process_batch)(batch) for batch in tqdm(train_loader)
     )
 
@@ -131,12 +137,25 @@ def cp_features(
     # TODO: need to handle nans in a smart way
     all_features_df = all_features_df.fillna(0)
 
-    save_cp_features(experiment_dict, all_features_df, override=True)
+    # save_cp_features(experiment_dict, all_features_df, override=True)
 
     return all_features_df
 
 
+def trim_features():
+    """
+    Some CP-measure features dont make sense to use in the context of our analysis,
+    these must be removed before downstream tasks
+    """
+
+    # remove BoundingBoxMinimum/Maximum
+    # Orientation
+
+    return
+
+
 if __name__ == "__main__":
     experiment_dict = {"ops0033_20250429": ["A/1/0", "A/2/0", "A/3/0"]}
-    cp_features(experiment_dict=experiment_dict, verbose=True, num_workers=15)
+    cp_features(experiment_dict=experiment_dict, verbose=True, num_workers=1)
+
 # %%
